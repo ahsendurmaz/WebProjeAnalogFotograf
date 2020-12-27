aa60d74034bb278c6629274b8cdaecd6adc595f9
sefaekici <ssefaekici@gmail.com>
2020-12-27 18:20:26 +0300

    Proje dosyası ekle.

---
+++ WebProjeAnalogFotograf/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebProjeAnalogFotograf.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
